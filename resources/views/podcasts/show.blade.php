34c7e17246a4a07b097a4816b5afcde51e7239c6
Ashley7 <ashley7520charles@gmail.com>
2026-03-20 17:37:12 +0300

    Clean ups

--- resources/views/podcasts/show.blade.php
+++ resources/views/podcasts/show.blade.php
@@ -83,6 +83,10 @@
         </div>
     @endforeach
 
+    <div class="container">
+        <div id="disqus_thread"></div>
+    </div>
+
 </div>
 
 {{-- Wavesurfer.js --}}
@@ -124,5 +128,22 @@
             return `${m.toString().padStart(2,'0')}:${s.toString().padStart(2,'0')}`;
         }
     @endif
+
 </script>
+
+<script>
+    var disqus_config = function () {
+    this.page.url = {{url()->current();}};  
+    this.page.identifier = {{ $podcast->id }};
+    };
+   
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://web-radio-frenchizly.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
 @endsection
